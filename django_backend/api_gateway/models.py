b78dddb3368d400e8ba070411c36f949fa4d8e60
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-05-09 03:31:21 +0530

    feat: link Tenant model to User

--- django_backend/api_gateway/models.py
+++ django_backend/api_gateway/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Tenant(models.Model):
     name = models.CharField(max_length=255) # client company's name
     api_key = models.CharField(max_length=255, unique = True) # To verify the company 
     created_at = models.DateTimeField(auto_now_add = True) #audit/logging purpose
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null = True, blank = True)
 
     def __str__(self):
         return self.name
