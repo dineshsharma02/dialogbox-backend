f2eb911a9470dec4b992f3537f9b392d932d8d6c
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-05-08 23:28:39 +0530

    feat: add tenant and FAQ models

--- django_backend/api_gateway/models.py
+++ django_backend/api_gateway/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tenant(models.Model):
+    name = models.CharField(max_length=255) # client company's name
+    api_key = models.CharField(max_length=255, unique = True) # To verify the company 
+    created_at = models.DateTimeField(auto_now_add = True) #audit/logging purpose
+
+    def __str__(self):
+        return self.name
