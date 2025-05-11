75a17bd42b7ace0deea82aec83f99852ccb7905f
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-05-12 03:57:54 +0530

    test: embed test FAQs for multiple tenants into ChromaDB

---
+++ django_backend/api_gateway/embedding/visualize_embeddings.py
@@ -0,0 +1,23 @@
+import chromadb
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../.."))
+CHROMA_PATH = os.path.join(BASE_DIR, "chroma_store")
+client = chromadb.PersistentClient(CHROMA_PATH)
+# collection = client.get_or_create_collection(name="faq_store")
+# client.delete_collection(name="faq_store")
+collection = client.get_or_create_collection(name="faq_store") 
+
+
+
+# Get all vectors (or filter by tenant)
+results = collection.get(
+    # where={"tenant_id": "t1_demo"},  # or remove this to get all
+    include=["documents", "embeddings", "metadatas"]
+)
+
+
+print("IDs:", results["ids"])
+print("Docs:", results["documents"])
+print("Embeddings:", results["embeddings"])
+print("metadatas:",results["metadatas"])
--- django_backend/api_gateway/vector/chroma_service.py
+++ django_backend/api_gateway/vector/chroma_service.py
@@ -1,6 +1,14 @@
 import chromadb
+import os
 
-client = chromadb.PersistentClient(path="./chroma_store")
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../.."))
+CHROMA_PATH = os.path.join(BASE_DIR, "chroma_store")
+
+
+client = chromadb.PersistentClient(path=CHROMA_PATH)
+
+
 
 
 collection = client.get_or_create_collection(name="faq_store")
@@ -9,10 +17,11 @@ collection = client.get_or_create_collection(name="faq_store")
 def add_documents(tenant_id: str, docs: list[str], embeddings: list[list[float]], ids: list[str]):
     metadatas = [{"tenant_id": tenant_id} for _ in docs]
     collection.add(documents=docs, embeddings=embeddings, metadatas=metadatas, ids=ids)
+    
 
 
 
-def query_documents(tenant_id: str, query_embedding: list[float], top_k=3):
+def query_documents(tenant_id: int, query_embedding: list[float], top_k=3):
     return collection.query(
         query_embeddings=[query_embedding],
         n_results=top_k,
