59cab405e4e583390d3de33a0856638b3154217f
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-05-12 17:31:17 +0530

    test: run full end-to-end retrieval tests for known queries

--- django_backend/api_gateway/views.py
+++ django_backend/api_gateway/views.py
@@ -7,6 +7,7 @@ from .models import Tenant
 from .utils import TenantContextMixin
 from rest_framework.response import Response
 from .user_query_pipeline import process_user_query
+import time
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -27,14 +28,18 @@ class HelloWorldView(TenantContextMixin,APIView):
 class UserQueryView(TenantContextMixin, APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request):
+        start_time = time.time()
         serializer = UserQuerySerializer(data = request.data)
         serializer.is_valid(raise_exception=True)
         tenant = self.get_tenant(request)
         question = serializer.validated_data['question']
         result = process_user_query(question, tenant.id)
+        end_time = time.time()
+        latency_ms = round((end_time - start_time) * 1000, 2)
 
         return Response({
             "success": True,
+            "latency":latency_ms,
             "data": {
                 "query": result["query"],
                 "matched_answers": result["top_results"],
