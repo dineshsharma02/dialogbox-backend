b5658585732eb9d7267d55f0fa8301d45b2fe28d
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-05-20 03:08:06 +0530

    feat: add Dockerized FastAPI embedding service for sentence transformers

---
+++ django_backend/embedding_service/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from sentence_transformers import SentenceTransformer
+import os
+
+MODEL_NAME = "BAAI/bge-base-en-v1.5"
+
+app = FastAPI()
+
+model = SentenceTransformer(MODEL_NAME)
+
+class TextRequest(BaseModel):
+    texts: list[str]
+    is_query:bool = False
+
+
+@app.post("/embed")
+async def embed_text(req: TextRequest):
+    prefix = "query: " if req.is_query else "passage: "
+    prepped = [prefix + text.strip() for text in req.texts]
+    embeddings = model.encode(prepped,normalize_embeddings=True)
+    return {"embeddings": embeddings}
