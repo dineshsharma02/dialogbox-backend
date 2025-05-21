68b213468e6a00ac190b57ab39294813bb8a4d4e
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-05-21 23:48:11 +0530

    chore: added backend service to docker

---
+++ test.py
@@ -0,0 +1,8 @@
+import chromadb
+chroma_client = chromadb.HttpClient(host='localhost', port=8000)
+
+collection = chroma_client.get_collection("faq_store")
+print(collection.count())
+
+
+print(chroma_client.heartbeat())
