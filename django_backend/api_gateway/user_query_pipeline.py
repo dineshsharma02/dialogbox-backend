fde2645ab8001fc6854a6cdd065fdc8648895e73
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-05-28 19:20:30 +0530

    fix: prompt fixes and reduced threshold

--- django_backend/api_gateway/user_query_pipeline.py
+++ django_backend/api_gateway/user_query_pipeline.py
@@ -21,15 +21,15 @@ def process_user_query(question: str, tenant_id: int):
     #         "context_used": [],
     #     }, status=400)
     retrieval = retrieve_top_k_answers(cleaned, tenant_id)
-    ranked_results = rank_results(retrieval, threshold=0.50)
-    # print(ranked_results)
-    # if not ranked_results:
-    #     return {
-    #         "query": cleaned,
-    #         "answer": "Sorry, I couldn’t find any relevant answer based on current information.",
-    #         "context_used": [],
-    #         "status": "no_match"
-    #     }
+    ranked_results = rank_results(retrieval, threshold=0.35)
+   
+    if not ranked_results:
+        return {
+            "query": cleaned,
+            "answer": "Sorry, I couldn’t find any relevant answer based on current information.",
+            "context_used": [],
+            "status": "no_match"
+    }
     
 
     context_docs = [item["text"] for item in ranked_results]
