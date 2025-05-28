6f445f0d86ae8e20e62c694dc5695acd2479e682
dineshsharma02 <dineshsharma18987374@gmail.com>
2025-05-29 00:32:28 +0530

    feat: Add instructions field and category relation to Tenant model

--- django_backend/api_gateway/admin.py
+++ django_backend/api_gateway/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Tenant
+from .models import Tenant, CompanyCategory
 
 @admin.register(Tenant)
 
 class TenantAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "created_at")
+    list_display = ("id", "name", "category", "created_at")
+    list_filter = ("category",)
     search_fields = ("id", "name")
+    fields = (..., "category", "instructions")
 
     
+@admin.register(CompanyCategory)
+class CompanyCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
